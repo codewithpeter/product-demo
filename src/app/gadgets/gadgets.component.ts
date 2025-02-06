7452cfc45ca63654e05d71988dd85b1665896990
Anupam Pradhan <anupamnasa505@gmail.com>
2025-02-06 23:17:38 +0530

    demo

---
+++ src/app/gadgets/gadgets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gadgets',
+  imports: [],
+  templateUrl: './gadgets.component.html',
+  styleUrl: './gadgets.component.css'
+})
+export class GadgetsComponent {
+
+}
